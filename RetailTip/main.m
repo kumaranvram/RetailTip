9929f668b83f62a14409e7e91c24214ae2f747e0
Senthil Kumar <senthil.vijay.kumar@gmail.com>
2012-02-15 00:34:23 +0530

    Initial Commit

---
+++ RetailTip/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  RetailTip
+//
+//  Created by Senthil Kumar Vijayakumar on 15/02/12.
+//  Copyright (c) 2012 ThoughtWorks. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
